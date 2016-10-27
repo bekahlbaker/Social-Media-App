d1eb0f7111ae1b17cc779e15cd1780d1d089b46d
bekahlbaker <bekahlbaker@gmail.com>
2016-10-27 12:27:24 -0500

    Added Facebook and Firebase authentication

--- Social Media App/SignInVC.swift
+++ Social Media App/SignInVC.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
-class SignInVC: UIViewController {
+class SignInVC: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: RoundButton) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("BEKAH: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("BEKAH: User cancelled Facebook authentication")
+            } else {
+                print("BEKAH: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+    }
+    
+    func firebaseAuthenticate(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("BEKAH: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("BEKAH: Successfully autheticated with FIrebase")
+            }
+        })
+    }
 }
 
