cd18867912815fbb7f06d788cb2de053619f5a83
bekahlbaker <bekahlbaker@gmail.com>
2016-10-28 11:54:45 -0500

    Complete tableview cell

---
+++ Social Media App/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  Social Media App
+//
+//  Created by Rebekah Baker on 10/28/16.
+//  Copyright © 2016 Rebekah Baker. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
