d70099d9e3640cb54ef17c1713cb5d9031dee3cc
bekahlbaker <bekahlbaker@gmail.com>
2016-11-01 11:04:42 -0500

    Feed UI complete

--- Social Media App/CircleImage.swift
+++ Social Media App/CircleImage.swift
@@ -9,21 +9,11 @@
 import UIKit
 
 class CircleImage: UIImageView {
-
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        self.layer.shadowOpacity = 0.5
-        self.layer.shadowRadius = 3.0
-        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
-        self.layer.shadowColor = UIColor.gray.cgColor
-    }
     
     override func layoutSubviews() {
-        super.layoutSubviews()
-        
         layer.cornerRadius = self.frame.width / 2
+        clipsToBounds = true
     }
-
+    
 }
+
